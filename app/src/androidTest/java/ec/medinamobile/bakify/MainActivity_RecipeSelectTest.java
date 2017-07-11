f0981ec49ba4b48290095cf368da6ace96eda1c4
eamedina87 <erickmedinamoreno@hotmail.com>
2017-07-10 23:42:54 -0500

    Test improvements

--- app/src/androidTest/java/ec/medinamobile/bakify/MainActivity_RecipeSelectTest.java
+++ app/src/androidTest/java/ec/medinamobile/bakify/MainActivity_RecipeSelectTest.java
@@ -2,9 +2,14 @@ package ec.medinamobile.bakify;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
+import android.support.v7.widget.Toolbar;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,8 +29,10 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.not;
@@ -41,6 +48,7 @@ public class MainActivity_RecipeSelectTest {
 
     public static final String INGREDIENTS_TITLE = "Ingredients (10)";
     public static final String STEPS_TITLE = "Steps (13)";
+    public static final String RECIPE_NAME = "Yellow Cake";
 
     @Rule public ActivityTestRule<MainActivity> mActivityTestRule
             = new ActivityTestRule<MainActivity>(MainActivity.class);
@@ -56,9 +64,28 @@ public class MainActivity_RecipeSelectTest {
         onView(withId(R.id.main_recyclerview)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
         onView(withId(R.id.detail_ingredients_title)).check(matches(withText(INGREDIENTS_TITLE)));
         onView(withId(R.id.detail_steps_title)).check(matches(withText(STEPS_TITLE)));
+        matchToolbarTitle(RECIPE_NAME);
 
     }
 
+    private static ViewInteraction matchToolbarTitle(
+            CharSequence title) {
+        return onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+    }
+
+    private static Matcher<Object> withToolbarTitle(
+            final Matcher<CharSequence> textMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+            @Override public boolean matchesSafely(Toolbar toolbar) {
+                return textMatcher.matches(toolbar.getTitle());
+            }
+            @Override public void describeTo(Description description) {
+                description.appendText("with toolbar title: ");
+                textMatcher.describeTo(description);
+            }
+        };
+    }
 
 
 
